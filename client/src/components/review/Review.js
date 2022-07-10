3e99b37fd203123c57aec6e12d4b94e539eb67fe
EsotericZ <cjsand03@gmail.com>
2022-07-10 17:57:06 -0600

    added form for new reviews

--- client/src/components/review/Review.js
+++ client/src/components/review/Review.js
@@ -1,12 +1,26 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { CREATE_REVIEW } from '../../graphql/mutations/createReview';
 import { useMutation } from '@apollo/client';
-import { CREATE_REVIEW } from '../../graphql/mutations/createReview';
+import { useNavigate } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControl from '@mui/material/FormControl';
+import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 import Nav from "../nav/Nav";
 import "./review.css";
 
 const Review = () => {
+    const ratingList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     const [formState, setFormState] = useState({
         title: '',
         plot: '',
@@ -17,7 +31,7 @@ const Review = () => {
         rating: '',
         review: '',
     })
-    const [createReviewMutation, {_data, _loading, error}] = useMutation(CREATE_REVIEW);
+    // const [createReviewMutation, {_data, _loading, error}] = useMutation(CREATE_REVIEW);
     const navigate = useNavigate();
 
     const handleChange = (event) => {
@@ -41,34 +55,151 @@ const Review = () => {
             review: formState.review,
         };
         console.log(body);
-        try {
-            let res = await createReviewMutation({
-                variables: body
-            });
-            navigate('/home');
-            if (!error) {
-                setFormState({
-                    title: '',
-                    plot: '',
-                    genre: '',
-                    year: '',
-                    poster: '',
-                    director: '',
-                    rating: '',
-                    review: '',
-                })
-            } else {
-                console.error(error)
-            }
-        } catch (err) {
-            console.log(err);
-        }
+        // try {
+        //     let res = await createReviewMutation({
+        //         variables: body
+        //     });
+        //     navigate('/home');
+        //     if (!error) {
+        //         setFormState({
+        //             title: '',
+        //             plot: '',
+        //             genre: '',
+        //             year: '',
+        //             poster: '',
+        //             director: '',
+        //             rating: '',
+        //             review: '',
+        //         })
+        //     } else {
+        //         console.error(error)
+        //     }
+        // } catch (err) {
+        //     console.log(err);
+        // }
     };
 
     return (
         <>
             <Nav />
-            <h1>New Review</h1>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+                            Create a New Review
+                        </Typography>
+                        <form noValidate onSubmit={(e) => handleSubmit(e)} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="title"
+                                        id="title"
+                                        label="Movie Title"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="plot"
+                                        id="plot"
+                                        label="Plot"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="genre"
+                                        id="genre"
+                                        label="Genre"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="year"
+                                        id="year"
+                                        label="Year"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="poster"
+                                        id="poster"
+                                        label="Poster URL"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="director"
+                                        id="director"
+                                        label="Director"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControl sx={{ width: 550 }}>
+                                        <InputLabel id="">Rating *</InputLabel>
+                                        <Select
+                                            required
+                                            fullWidth
+                                            name="rating"
+                                            labelId="rating"
+                                            id=""
+                                            value={formState.rating}
+                                            onChange={handleChange}
+                                        >
+                                        {ratingList.map((rating) => (
+                                            <MenuItem key={rating} value={rating}>
+                                                {rating}
+                                            </MenuItem>
+                                        ))}
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        name="review"
+                                        id="review"
+                                        label="Review"
+                                        autoFocus
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button className="createBtn"
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mb: 2 }}
+                                
+                            >
+                                Submit
+                            </Button>
+                        </form>
+                    </Box>
+                </Container>
         </>
     )
 }
