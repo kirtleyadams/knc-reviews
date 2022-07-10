b6fa73a07a6477815f5a1db0bcf5dffbe4113bb2
EsotericZ <cjsand03@gmail.com>
2022-07-10 16:35:49 -0600

    added backend to reviews page

--- client/src/components/review/Review.js
+++ client/src/components/review/Review.js
@@ -1,7 +1,70 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { CREATE_REVIEW } from '../../graphql/mutations/createReview';
+
 import Nav from "../nav/Nav";
 import "./review.css";
 
 const Review = () => {
+    const [formState, setFormState] = useState({
+        title: '',
+        plot: '',
+        genre: '',
+        year: '',
+        poster: '',
+        director: '',
+        rating: '',
+        review: '',
+    })
+    const [createReviewMutation, {_data, _loading, error}] = useMutation(CREATE_REVIEW);
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const body = {
+            title: formState.title,
+            plot: formState.plot, 
+            genre: formState.genre,
+            year: formState.year,
+            poster: formState.posters,
+            director: formState.director,
+            rating: formState.rating,
+            review: formState.review,
+        };
+        console.log(body);
+        try {
+            let res = await createReviewMutation({
+                variables: body
+            });
+            navigate('/home');
+            if (!error) {
+                setFormState({
+                    title: '',
+                    plot: '',
+                    genre: '',
+                    year: '',
+                    poster: '',
+                    director: '',
+                    rating: '',
+                    review: '',
+                })
+            } else {
+                console.error(error)
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <>
             <Nav />
---
+++ client/src/graphql/mutations/createReview.js
@@ -0,0 +1,8 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_REVIEW = gql`
+mutation createReview($title: String!, $plot: String!, $genre: String!, $year: Int!, $poster: String, $director: String, $rating: String, $review: String!) {
+  createReview(title: $title, plot: $plot, genre: $genre, year: $year, poster: $poster, director: $director, rating: $rating, review: $review) {
+  }
+}
+`;
